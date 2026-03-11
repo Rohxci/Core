9500b3c8a851c20e7d5f2e43f81d9c77763f1a1a
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-11 11:26:59 +0100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,9 +4,13 @@ module.exports = {
   CHANNEL_CATEGORIES: [
     "profile",
     "economy",
-    "shop",
     "leaderboard",
     "level"
+  ],
+
+  SHOP_TYPES: [
+    "normal",
+    "seasonal"
   ],
 
   SHOP_ITEM_TYPES: [
@@ -27,15 +31,38 @@ module.exports = {
     "economy"
   ],
 
+  SEASON_STATUSES: [
+    "active",
+    "ended"
+  ],
+
+  SEASON_PASS_REWARD_TYPES: [
+    "coins",
+    "season_coins",
+    "role",
+    "badge",
+    "inventory"
+  ],
+
   DEFAULTS: {
+    currentSeason: 0,
+    seasonStatus: "active",
+
     currencyName: "Coins",
     currencySymbol: "🪙",
+
+    seasonCurrencyName: "Season Coins",
+    seasonCurrencySymbol: "❄️",
 
     dailyMin: 100,
     dailyMax: 250,
 
     workMin: 50,
     workMax: 150,
+
+    seasonDailyAmount: 10,
+    seasonWorkAmount: 5,
+    seasonLevelupAmount: 3,
 
     messageXpMin: 4,
     messageXpMax: 7,
@@ -52,12 +79,20 @@ module.exports = {
 
     leaderboardSize: 10,
     profileBadgesPreview: 8,
-    shopSelectMaxOptions: 25
+    shopSelectMaxOptions: 25,
+    panelTextMaxLength: 2000
   },
 
   COOLDOWNS: {
     repGiveMs: 24 * 60 * 60 * 1000,
     dailyMs: 24 * 60 * 60 * 1000,
     workMs: 12 * 60 * 60 * 1000
-  }
+  },
+
+  TIMERS: {
+    profileDeleteMs: 5 * 60 * 1000,
+    leaderboardDeleteMs: 5 * 60 * 1000
+  },
+
+  LEVEL_UP_COINS_REWARD: 50
 };
