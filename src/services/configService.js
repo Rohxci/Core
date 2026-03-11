d4d3431d788e30d6636247acec8414de650eca73
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-11 11:28:13 +0100

    Update configService.js

--- src/services/configService.js
+++ src/services/configService.js
@@ -5,12 +5,19 @@ async function ensureGuildSettings(guildId) {
   await pool.query(
     `INSERT INTO guild_settings (
       guild_id,
+      current_season,
+      season_status,
       currency_name,
       currency_symbol,
+      season_currency_name,
+      season_currency_symbol,
       daily_min,
       daily_max,
       work_min,
       work_max,
+      season_daily_amount,
+      season_work_amount,
+      season_levelup_amount,
       message_xp_min,
       message_xp_max,
       message_xp_cooldown_seconds,
@@ -18,17 +25,24 @@ async function ensureGuildSettings(guildId) {
       voice_xp_interval_minutes
     )
     VALUES (
-      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19
     )
     ON CONFLICT (guild_id) DO NOTHING`,
     [
       guildId,
+      DEFAULTS.currentSeason,
+      DEFAULTS.seasonStatus,
       DEFAULTS.currencyName,
       DEFAULTS.currencySymbol,
+      DEFAULTS.seasonCurrencyName,
+      DEFAULTS.seasonCurrencySymbol,
       DEFAULTS.dailyMin,
       DEFAULTS.dailyMax,
       DEFAULTS.workMin,
       DEFAULTS.workMax,
+      DEFAULTS.seasonDailyAmount,
+      DEFAULTS.seasonWorkAmount,
+      DEFAULTS.seasonLevelupAmount,
       DEFAULTS.messageXpMin,
       DEFAULTS.messageXpMax,
       DEFAULTS.messageXpCooldownSeconds,
@@ -52,7 +66,60 @@ async function getGuildSettings(guildId) {
   return result.rows[0];
 }
 
+async function updateGuildSetting(guildId, field, value) {
+  const allowedFields = [
+    "profiles_enabled",
+    "reputation_enabled",
+    "economy_enabled",
+    "shop_enabled",
+    "levels_enabled",
+    "current_season",
+    "season_status",
+    "currency_name",
+    "currency_symbol",
+    "season_currency_name",
+    "season_currency_symbol",
+    "daily_min",
+    "daily_max",
+    "work_min",
+    "work_max",
+    "season_daily_amount",
+    "season_work_amount",
+    "season_levelup_amount",
+    "message_xp_min",
+    "message_xp_max",
+    "message_xp_cooldown_seconds",
+    "voice_xp_amount",
+    "voice_xp_interval_minutes",
+    "levelup_messages_enabled",
+    "normal_shop_panel_channel_id",
+    "normal_shop_panel_message_id",
+    "seasonal_shop_panel_channel_id",
+    "seasonal_shop_panel_message_id",
+    "season_pass_panel_channel_id",
+    "season_pass_panel_message_id"
+  ];
+
+  if (!allowedFields.includes(field)) {
+    throw new Error("Invalid guild setting field.");
+  }
+
+  await ensureGuildSettings(guildId);
+
+  const result = await pool.query(
+    `UPDATE guild_settings
+     SET ${field} = $2,
+         updated_at = NOW()
+     WHERE guild_id = $1
+     RETURNING *`,
+    [guildId, value]
+  );
+
+  return result.rows[0];
+}
+
 module.exports = {
   ensureGuildSettings,
-  getGuildSettings
+  getGuildSettings,
+  updateGuildSetting
 };
